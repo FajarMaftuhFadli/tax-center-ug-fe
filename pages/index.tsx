48fa29b34d067eb577aad27b53792361442abcf1
Fajar Maftuh Fadli <fajarmaftuhfadli@gmail.com>
2022-12-28 16:06:07 +0700

    feat: add layout (navbar and footer) system to pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 import Head from "next/head";
+import type { ReactElement } from "react";
+import Layout from "../components/Layout";
+import type { NextPageWithLayout } from "./_app";
 
-export default function Home() {
+const Home: NextPageWithLayout = () => {
   return (
     <>
       <Head>
@@ -9,7 +12,19 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main></main>
+      <main>
+        <div className="p-40">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum tempora
+          reiciendis nesciunt doloremque recusandae illo porro vitae, sint totam
+          odit sequi non ad itaque quos tempore animi voluptatum vel vero!
+        </div>
+      </main>
     </>
   );
-}
+};
+
+Home.getLayout = function getLayout(page: ReactElement) {
+  return <Layout>{page}</Layout>;
+};
+
+export default Home;
